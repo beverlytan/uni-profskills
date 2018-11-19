28be9bc9aa2c7ca8c6675335fb372d0642a254fd
beverlytan <s1550222@sms.ed.ac.uk>
2018-11-19 21:25:12 +0000

    Beverly might have fixed it!!

--- 03-glm/glm-further-script3.R
+++ 03-glm/glm-further-script3.R
@@ -15,42 +15,39 @@ soils <- read_csv("02-multiple-predictors/peru_soil_data.csv")
 inga <- read_csv("03-glm/inga_abundances.csv")
 combo <- full_join(soils, inga, by = "Survey")
 
-# Poisson model, 2 explanatory fixed effects, no interaction ---- 
+# TESTING ONE Poisson model, 2 explanatory fixed effects, no interaction ---- 
 
 mod1 <- glm(thibaudiana ~ Habitat + Soil_pH, data = combo, family = poisson)
 summary(mod1)
 
+## USING GGPREDICT()
+
 gg_preds <- ggpredict(mod1, terms = c("Soil_pH", "Habitat"))
+View(gg_preds)
+
 # Note here that the first term is grouped by the levels of the second
 # and if you have a third, and the third
 # meaning the second and the third are GROUPS! Third = facet in the table
 # This is useful https://strengejacke.github.io/ggeffects/reference/ggpredict.html 
 
-View(gg_preds)
+## USING FITTED()
+
+fit_preds <- as.data.frame(fitted(mod1))
+combo_mod1 <- cbind(fit_preds, combo)
+View(combo_mod1)
 
 # If I use fitted(mod1) and merge that to my original dataframe 
-# I realise that it's actually the same!! 
-fit_preds <- as.data.frame(fitted(mod1))
+# I realise that it's actually the same!! O M G fit_preds = gg_preds!!!
+# What I was doing BEFORE THIS was ggpredict(mod1, terms = "Soil_pH")
+# or ggpredict(mod1, terms = "Habitat") and that just looks at 
+# ONE explanatory variable only!!
 
-test <- cbind(fit_preds, combo)
-
-View(test)
+## ASIDE: DIFFERENCE BT FITTED() AND PREDICT()
 
 as.data.frame(predict(mod1))
 exp(-1.710389) # 0.1807954 Shows that if you exp(predict_values) = fitted_values
 
-
-# Poisson model, 2 explanatory fixed effects, no interaction ---- 
-
-
-
-
-
-ggpredict(mod1)
-ggpredict(mod1, terms = "Habitat") # The default here is type = "fe" (fixed)
-ggpredict(mod1, terms = "Soil_pH")
-
-# Poisson model, 2 explanatory fixed effects, with interaction ---- 
+# TESTING TWO Poisson model, 2 explanatory fixed effects, with interaction ---- 
 
 mod2 <- lm(thibaudiana ~ Habitat * Soil_pH, data = combo)
 summary(mod2)
